c3977be222af64a45c6e24b1dd98fc464023f629
Miguel Torrinha <miguel.torrinha.pereira+github@gmail.com>
2026-07-16 18:44:38 +0100

    Forget installing stuff

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ git clone https://github.com/laura-sofia/pbr.git
 mv ~/pbr/linux_setup ~/atividade_linux
 rm -rf ~/pbr
 
-sudo apt -y update && sudo apt -y upgrade && sudo apt install cmatrix tree btop htop
+# sudo echo 'deb http://archive.raspberrypi.org/debian/ buster main' > /etc/apt/sources.list
+# sudo apt -y update && sudo apt -y upgrade && sudo apt install cmatrix tree btop htop

9c1154e700c91e42e3541d5ae6b1a509535488cf
laura-sofia <20laurasofia06@gmail.com>
2026-07-16 15:53:09 +0100

    run.sh added btop

--- linux_setup/run.sh
+++ linux_setup/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mv pbr/linux_setup atividade_linux
-rm -r pbr
-sudo apt update < yes
-sudo apt upgrade < yes
-sudo apt install cmatrix
-sudo apt install tree
+rm -rf pbr
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install cmatrix tree btop htop

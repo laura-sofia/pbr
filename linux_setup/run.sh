472d0a2be925201b3e78a278fa8945da94ab03d9
laura-sofia <20laurasofia06@gmail.com>
2026-07-16 15:26:43 +0100

    run.sh added mv and rm

--- linux_setup/run.sh
+++ linux_setup/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-sudo apt update
-sudo apt upgrade
+mv pbr/linux_setup atividade_linux
+rm -r pbr
+sudo apt update < yes
+sudo apt upgrade < yes
 sudo apt install cmatrix
 sudo apt install tree
